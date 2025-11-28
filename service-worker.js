1f070ca9b6d26489821bb9a68fefb1e11c6387f9
Kattiekatezzn <deverajessy58@gmail.com>
2025-11-28 10:03:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,16 @@
-const CACHE = 'tpk-analyzer-cache-v1';
-const FILES = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+const CACHE = "tpk-analyzer-cache-v1";
+const FILES = ["/", "/index.html", "/style.css", "/script.js", "/manifest.json"];
+
+self.addEventListener("install", evt=>{
+  evt.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
   self.skipWaiting();
 });
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE) return caches.delete(k); }))));
+
+self.addEventListener("activate", evt=>{
+  evt.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{ if(k!==CACHE) return caches.delete(k); }))));
   self.clients.claim();
 });
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
+
+self.addEventListener("fetch", evt=>{
+  evt.respondWith(caches.match(evt.request).then(r=>r || fetch(evt.request)));
 });
